531ed16947f4512b57716bd5cce1c583b3ed66de
Elias Reis <eliasreis54@gmail.com>
2021-01-24 21:09:05 -0300

    add auth middleware

---
+++ functions/server/src/middleware/index.js
@@ -0,0 +1,26 @@
+const admin = require('firebase-admin')
+
+const authMiddleware = async (req, res, next) => {
+  const { authorization } = req.headers
+  if (!authorization) {
+    return res.status(401).json({ message: 'You must send authorization' })
+  }
+
+  const token = authorization.split(' ')
+  if (token[0] !== 'Bearer') {
+    return res.status(401).json({ message: 'You must send Bearer token' })
+  }
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token[1])
+    req.user = decodedToken
+    return next()
+  } catch (e) {
+    console.log(e)
+    return res.sendStatus(401)
+  }
+}
+
+
+module.exports = {
+  authMiddleware
+}
